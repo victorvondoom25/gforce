ba009a315ebf3f6b03bdbdcd4e529c698c3fa1d8
Siddharth Jain <siddharthchess25@gmail.com>
2026-07-19 11:39:53 +0530

    Increase default MoveOverhead to 250ms for cloud hosting

--- src/engine/UCI.hpp
+++ src/engine/UCI.hpp
@@ -13,7 +13,7 @@ struct Options
 {
     uint32_t multiPV = 1;
     uint32_t threads = 1;
-    int32_t moveOverhead = 10;
+    int32_t moveOverhead = 250;
     int32_t evalRandomization = 0;
     bool analysisMode = false;
     bool useStandardAlgebraicNotation = false;
